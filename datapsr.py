f64e72d833dc06f4106e66a2ad779ac9c26f3d72
ZhuangweiKang <zkty9@mail.missouri.edu>
2020-12-03 23:03:22 -0600

    update

--- datapsr.py
+++ datapsr.py
@@ -46,3 +46,36 @@ def find_line(fname):
         for l in f.readlines():
             if 'Length' in l:
                 return l
+
+
+def load_data(tests, plugins, latencyTest=False):
+    throughput_perf = []
+    latency_perf = []
+    if latencyTest:
+        test = 'latency-test'
+    else:
+        test = 'throughput-test'
+    for cni in plugins:
+        for t in tests:
+            perfs = []
+            subs = [sub for sub in os.listdir('data/%s/%s/test-%d/' % (test, cni, t)) if 'sub' in sub]
+            for sub in subs:
+                perftest_output = find_line('data/%s/%s/test-%d/%s' % (test, cni, t, sub))
+                tperf = parse_throughput(perftest_output)
+                perfs.append(tperf)
+            avg_perf = {}
+            for fld in perfs[0]:
+                avg_perf.update({fld: 0})
+            for fld in perfs[0]:
+                for perf in perfs:
+                    avg_perf[fld] += perf[fld]
+            for fld in avg_perf:
+                avg_perf[fld] /= len(subs)
+            avg_perf.update({'test': t, 'cni': cni})
+            throughput_perf.append(avg_perf)
+
+            perftest_output = find_line('data/%s/%s/test-%d/rtiperftest-pub0.log' % (test, cni, t))
+            lperf = parse_latency(perftest_output)
+            lperf.update({'test': t, 'cni': cni})
+            latency_perf.append(lperf)
+    return pd.DataFrame(throughput_perf), pd.DataFrame(latency_perf)      
